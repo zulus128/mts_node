c6ed4c72dae88dee74bf9a8743bac9e1773e396c
VADIM KASSIN <vgzulus@gmail.com>
2016-02-26 22:01:05 +0600

    1st commit

---
+++ REST.js
@@ -0,0 +1,32 @@
+/**
+ * Created by vadimkassin on 2/26/16.
+ */
+
+var mysql = require("mysql");
+
+function REST_ROUTER(router,connection,md5) {
+    var self = this;
+    self.handleRoutes(router,connection,md5);
+}
+
+REST_ROUTER.prototype.handleRoutes= function(router,connection,md5) {
+
+    router.get("/",function(req,res){
+        res.json({"Message" : "Hello World !"});
+    });
+
+    router.post("/users",function(req,res){
+        var query = "INSERT INTO ??(??,??) VALUES (?,?)";
+        var table = ["user_login","user_email","user_password",req.body.email,md5(req.body.password)];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+            if(err) {
+                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+            } else {
+                res.json({"Error" : false, "Message" : "User Added !"});
+            }
+        });
+    });
+}
+
+module.exports = REST_ROUTER;
